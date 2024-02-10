1339dec1424109db580ac1259330e12a9df03e27
cactus1386 <cactusinjast@gmail.com>
2024-02-09 22:49:50 -0800

    debuged code

--- TahmasbBirth.py
+++ TahmasbBirth.py
@@ -1,25 +1,26 @@
 # Problem Source: https://quera.org/problemset/193462
 
+
 def BiggestPart(n, degree):
     a = []
     b = []
-    for m in range(n):
-        for word in degree.split():
-            a.append(float(word))
+    for word in degree.split():
+        a.append(float(word))
 
-        a = sorted(a)
-        for i in range(len(a)):
-            if i == 0:
-                b.append(a[i])
+    a = sorted(a)
+    for i in range(len(a)):
+        if i == 0:
+            b.append(a[i])
 
-            elif i == len(a) - 1:
-                b.append(360 - a[i])
-            else:
-                b.append(a[i] - a[i - 1])
-    
+        else:
+            b.append(a[i] - a[i - 1])
+
+        b.append(360 - a[-1])
+
     persent = max(b) / 3.6
     print(persent)
-    
+
+
 first = int(input())
 second = input()
 BiggestPart(first, second)
